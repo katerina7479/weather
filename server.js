db0e3866ea28dfa6b8e5dd38abefd3bfe2497655
Katerina Hanson <katerina.hanson@stridehealth.com>
2017-06-08 17:18:05 -0700

    remove extra consoles

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const PORT = process.env.PORT || 3000;
 // MIddleware to perform on every request
 // See if traffic is http, we're redirecting https (oooo)
 app.use(function (req, res, next) {
-  console.log(JSON.stringify(req.headers));
   var proto = req.headers['x-forwarded-proto']
-  console.log(proto);
   if (typeof (proto) == 'undefined' || (proto === 'http')) {
-    console.log("not redirecting");
     next();
   } else {
-    console.log("redirecting");
     res.redirect('http://' + req.hostname + req.url);
     next();
   }
