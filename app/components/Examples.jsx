1a8dc7a06bca858da2cf8d8a8c0a1de7e7b6dea3
Katerina Hanson <katerina.hanson@stridehealth.com>
2017-06-12 17:37:39 -0700

    Examples styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,23 @@
-var React = require('react')
+var React = require('react');
+var { Link } = require('react-router');
 
 
 var Examples = (props) => {
   return (
     <div>
-      <h1>Examples</h1>
-      <p>This is my examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location="San Francisco"'>San Francisco, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location="London, UK"'>London, UK</Link>
+        </li>
+        <li>
+          <Link to='/?location="Tokyo, Japan"'>Tokyo, Japan</Link>
+        </li>
+      </ol>
     </div>
   )
 };
